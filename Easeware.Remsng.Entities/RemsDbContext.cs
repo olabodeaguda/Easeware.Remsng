5170022ddc30f18009e1dc660fdf278841971d05
Aguda Olabode H <olabodeaguda@outlook.com>
2018-09-27 16:56:34 +0100

    update

---
+++ Easeware.Remsng.Entities/RemsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easeware.Remsng.Entities
+{
+    public class RemsDbContext : DbContext
+    {
+        public RemsDbContext(DbContextOptions<RemsDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ Easeware.Remsng.Entities/SeedData.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easeware.Remsng.Entities
+{
+    public class SeedData
+    {
+        public static void Initialized(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetRequiredService<RemsDbContext>();
+            context.Database.EnsureCreated();
+            context.Database.Migrate();
+        }
+    }
+}
